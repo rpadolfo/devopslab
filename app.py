60fbe04c47b79a23a9d79c0bbd0e911086b11961
Adolfo Paunksnis <rpadolfo@gmail.com>
2024-05-17 00:04:45 +0000

    Bug Removed

--- app.py
+++ app.py
@@ -7,12 +7,3 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "<H1>Olá! Como vai você?</H1>"
-
-@app.route('/bug')
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        print(e)
-    except TypeError as e:
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
